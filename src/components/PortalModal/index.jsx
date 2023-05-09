7dc3cc64d25c6841933094ac9cc2f97c2c55690a
MichaellDev1 <santuchomichael131@gmail.com>
2023-05-09 17:16:57 -0300

    Arreglo de eliminar usuario

--- src/components/PortalModal/index.jsx
+++ src/components/PortalModal/index.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { ContextConsumerAuth } from '../../context/AuthContext'
+
 const FormDelete = ({ handleSubmit, setModalDelete }) => {
   const [dataUser, setDataUser] = useState({
     email: '',
@@ -15,40 +17,50 @@ const FormDelete = ({ handleSubmit, setModalDelete }) => {
   }
 
   return (
-    <form className='w-full flex flex-col' onSubmit={(e) => handleSubmit({ e, data: dataUser })}>
-      <label htmlFor='' className='flex relative text-base flex-col text-neutral-400 font-normal mt-5'>
-        <span className='pl-3'>Email</span>
-        <input type='email' name='email' onChange={handleChangeInput} className='bg-white py-2 rounded-3xl border text-neutral-500 pl-10 pr-5  border-neutral-300' />
-      </label>
-      <label htmlFor='' className='flex relative text-base flex-col text-neutral-400 font-normal mt-5'>
-        <span className='pl-3'>Password</span>
-        <input type='password' name='password' onChange={handleChangeInput} className='bg-white py-2 rounded-3xl border text-neutral-500 pl-10 pr-5  border-neutral-300' />
-      </label>
-      <button className='bg-blue-600 hover:bg-blue-500 transition-[background-color] font-semibold uppercase text-white w-full py-2 rounded-3xl text-base'>Register</button>
-    </form>
+    <div className='w-full'>
+      <form className='w-full flex flex-col' onSubmit={(e) => handleSubmit({ e, data: dataUser })}>
+        <label htmlFor='' className='flex relative text-base flex-col text-neutral-400 font-normal mt-5'>
+          <span className='pl-3'>Email</span>
+          <input type='email' name='email' onChange={handleChangeInput} className='bg-white py-2 rounded-3xl border text-neutral-500 pl-10 pr-5  border-neutral-300' />
+        </label>
+        <label htmlFor='' className='flex relative text-base flex-col text-neutral-400 font-normal mt-5'>
+          <span className='pl-3'>Password</span>
+          <input type='password' name='password' onChange={handleChangeInput} className='bg-white py-2 rounded-3xl border text-neutral-500 pl-10 pr-5  border-neutral-300' />
+        </label>
+        <button className='bg-blue-600 hover:bg-blue-500 transition-[background-color] font-semibold uppercase text-white w-full py-2 rounded-3xl text-base mt-3'>Eliminar</button>
+      </form>
+      <button className='bg-neutral-300 hover:bg-neutral-400 transition-[background-color] font-semibold uppercase text-neutral-50 w-full py-2 rounded-3xl text-base mt-3' onClick={() => setModalDelete(false)}>Cancelar</button>
+    </div>
   )
 }
 
 export default function PortalModal ({ setModalDelete }) {
   const [isYes, setYes] = useState(false)
   const navigate = useNavigate()
+  const { setUser } = ContextConsumerAuth()
   const deleteAccount = () => {
     setYes(true)
   }
   const handleSubmit = ({ e, data }) => {
     e.preventDefault()
 
+    const token = window.localStorage.getItem('SESSION_ID')
+
     if (!data.email && !data.password) return
     fetch('http://localhost:4000/auth', {
       method: 'DELETE',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
       },
       body: JSON.stringify(data)
     }).then(res => {
-      window.localStorage.removeItem('sessionid')
-      return navigate('/login')
-    })
+      if (res.status === 200) {
+        setUser(null)
+        window.localStorage.removeItem('SESSION_ID')
+        return navigate('/login')
+      }
+    }).catch(e => console.log(e))
   }
 
   return (
